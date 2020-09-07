c08b3ca3677581e47b8fe307cd74e767906fd8a9
Ivan-Tang1MTFAZ <66385675+Ivan-Tang1MTFAZ@users.noreply.github.com>
2020-09-07 14:41:29 -0500

    Sexto commit

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
+import '../index.css';
 import {Link} from 'react-router-dom';
+import Guia from './Guia.png';
+import Dispositivos from './Dispositivos.png';
+import Configuracion from './Configuracion.png';
 
 class Menu extends Component {
     render(){
         return(
-          <div class="span-navbar-bottom Menu fixed-bottom">
+          <div class="span-navbar-bottom">
               
-              <nav class="navbar fixed-bottom">
+              <nav class="navbar fixed-bottom Menu">
                <ul>
-                   <Link to="/" class="nav-brand"><button class="btn inline">Guía</button></Link>
-                   <Link to="/" class="nav-brand"><button class="btn inline">Dispostivos</button></Link>
-                   <Link to="/" class="nav-brand"><button class="btn inline">Ajustes</button></Link>
+                   <Link to="/" class="btn2"><img src={Guia} alt="" width="10%" heigth="10%" class="imgM"></img></Link>
+                   <Link to="/" class="nav-brand btn3"><img src={Dispositivos} alt="" width="10%" heigth="10%" class="imgM"></img></Link>
+                   <Link to="/" class="nav-brand btn4"><img src={Configuracion} alt="" width="10%" heigth="10%" class="imgM"></img></Link>
                </ul>
            </nav>
               
--- src/pages/Principal.js
+++ src/pages/Principal.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import Encabezado_EW from '../component/Encabezado_EW';
 import Menu from '../component/Menu';
+import Anadir from '../component/Anadir.png';
+import {Link} from 'react-router-dom';
 
 class Principal extends Component {
     render(){
         return(
             <div>
-              
-                  <Encabezado_EW/>
-                  
-                      <label align="center" class="Titulo">Dispositivos</label>
+              <center>
+              <Encabezado_EW/>
+                        <br/>
+                      <label align="center" class="Titulo"><strong>Dispositivos</strong></label>
+                      <Link to="/"><img src={Anadir} alt="" width="15%" heigth="15%" class="imgM Anadir"></img></Link>
                       <Menu/>
-               
+              </center>
                
             </div>
         );
